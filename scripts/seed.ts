213b77e69d701afa8996df347c16aca53af7f4ee
daniel <danieledjv@gmail.com>
2024-01-16 03:51:58 -0600

    first commit

---
+++ scripts/seed.ts
@@ -0,0 +1,27 @@
+const { PrismaClient } = require("@prisma/client");
+
+const database = new PrismaClient();
+
+async function main() {
+  try {
+    await database.category.createMany({
+      data: [
+        { name: "Gastronomia" },
+        { name: "Musica" },
+        { name: "Fitness" },
+        { name: "Fotografia" },
+        { name: "Transformación Digital" },
+        { name: "Costura" },
+        { name: "Personalización Textil" }
+      ]
+    });
+
+    console.log("Success");
+  } catch (error) {
+    console.log("Error seeding the database categories", error);
+  } finally {
+    await database.$disconnect();
+  }
+}
+
+main();
