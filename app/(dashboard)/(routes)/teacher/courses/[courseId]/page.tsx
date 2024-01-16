2ff59e5f8b7e670f50be04a0b88b5d28efb46f61
daniel <danieledjv@gmail.com>
2024-01-16 14:37:15 -0600

    categorias

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,6 +1,11 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
-import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
+import {
+  CircleDollarSign,
+  File,
+  LayoutDashboard,
+  ListChecks
+} from "lucide-react";
 
 import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
@@ -15,11 +20,7 @@ import { AttachmentForm } from "./_components/attachment-form";
 import { ChaptersForm } from "./_components/chapters-form";
 import { Actions } from "./_components/actions";
 
-const CourseIdPage = async ({
-  params
-}: {
-  params: { courseId: string }
-}) => {
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
 
   if (!userId) {
@@ -34,21 +35,21 @@ const CourseIdPage = async ({
     include: {
       chapters: {
         orderBy: {
-          position: "asc",
-        },
+          position: "asc"
+        }
       },
       attachments: {
         orderBy: {
-          createdAt: "desc",
-        },
-      },
-    },
+          createdAt: "desc"
+        }
+      }
+    }
   });
 
   const categories = await db.category.findMany({
     orderBy: {
-      name: "asc",
-    },
+      name: "asc"
+    }
   });
 
   if (!course) {
@@ -61,7 +62,7 @@ const CourseIdPage = async ({
     course.imageUrl,
     course.price,
     course.categoryId,
-    course.chapters.some(chapter => chapter.isPublished),
+    course.chapters.some((chapter) => chapter.isPublished)
   ];
 
   const totalFields = requiredFields.length;
@@ -74,18 +75,14 @@ const CourseIdPage = async ({
   return (
     <>
       {!course.isPublished && (
-        <Banner
-          label="This course is unpublished. It will not be visible to the students."
-        />
+        <Banner label='This course is unpublished. It will not be visible to the students.' />
       )}
-      <div className="p-6">
-        <div className="flex items-center justify-between">
-          <div className="flex flex-col gap-y-2">
-            <h1 className="text-2xl font-medium">
-              Course setup
-            </h1>
-            <span className="text-sm text-slate-700">
-              Complete all fields {completionText}
+      <div className='p-6'>
+        <div className='flex items-center justify-between'>
+          <div className='flex flex-col gap-y-2'>
+            <h1 className='text-2xl font-medium'>Prepara tu Curso</h1>
+            <span className='text-sm text-slate-700'>
+              Completa todos los campos {completionText}
             </span>
           </div>
           <Actions
@@ -94,77 +91,51 @@ const CourseIdPage = async ({
             isPublished={course.isPublished}
           />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
           <div>
-            <div className="flex items-center gap-x-2">
+            <div className='flex items-center gap-x-2'>
               <IconBadge icon={LayoutDashboard} />
-              <h2 className="text-xl">
-                Customize your course
-              </h2>
+              <h2 className='text-xl'>Personaliza tu curso</h2>
             </div>
-            <TitleForm
-              initialData={course}
-              courseId={course.id}
-            />
-            <DescriptionForm
-              initialData={course}
-              courseId={course.id}
-            />
-            <ImageForm
-              initialData={course}
-              courseId={course.id}
-            />
+            <TitleForm initialData={course} courseId={course.id} />
+            <DescriptionForm initialData={course} courseId={course.id} />
+            <ImageForm initialData={course} courseId={course.id} />
             <CategoryForm
               initialData={course}
               courseId={course.id}
               options={categories.map((category) => ({
                 label: category.name,
-                value: category.id,
+                value: category.id
               }))}
             />
           </div>
-          <div className="space-y-6">
+          <div className='space-y-6'>
             <div>
-              <div className="flex items-center gap-x-2">
+              <div className='flex items-center gap-x-2'>
                 <IconBadge icon={ListChecks} />
-                <h2 className="text-xl">
-                  Course chapters
-                </h2>
+                <h2 className='text-xl'>Capitulos del curso</h2>
               </div>
-              <ChaptersForm
-                initialData={course}
-                courseId={course.id}
-              />
+              <ChaptersForm initialData={course} courseId={course.id} />
             </div>
             <div>
-              <div className="flex items-center gap-x-2">
+              <div className='flex items-center gap-x-2'>
                 <IconBadge icon={CircleDollarSign} />
-                <h2 className="text-xl">
-                  Sell your course
-                </h2>
+                <h2 className='text-xl'>Vende tu Curso</h2>
               </div>
-              <PriceForm
-                initialData={course}
-                courseId={course.id}
-              />
+              <PriceForm initialData={course} courseId={course.id} />
             </div>
             <div>
-              <div className="flex items-center gap-x-2">
+              <div className='flex items-center gap-x-2'>
                 <IconBadge icon={File} />
-                <h2 className="text-xl">
-                  Resources & Attachments
-                </h2>
+                <h2 className='text-xl'>Recursos y Archivos</h2>
               </div>
-              <AttachmentForm
-                initialData={course}
-                courseId={course.id}
-              />
+              <AttachmentForm initialData={course} courseId={course.id} />
             </div>
           </div>
         </div>
       </div>
     </>
-   );
-}
- 
+  );
+};
+
 export default CourseIdPage;
